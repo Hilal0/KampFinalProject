4fe16f0edc54413394b8d659137f60228a969e90
Hilal0 <hilalabadanoglu99@gmail.com>
2021-02-06 21:06:22 +0300

    KampIntro

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{ 
+    //generic constaint
+    //class: referans tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    public interface IEntityRepository<T> where T:class
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
